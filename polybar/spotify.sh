25a2450e261461ca801c2ef4024fde19a342cd8c
Joshua Campbell <joshuaccampbell@outlook.com>
2018-04-25 22:58:19 -0300

    Wed Apr 25 22:58:19 ADT 2018

---
+++ polybar/spotify.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$(playerctl status)" = "Playing" ]; then
+    song_title=`exec playerctl metadata xesam:title`
+    song_artist=`exec playerctl metadata xesam:artist`
+    echo "$song_title - $song_artist"
+else
+  echo ""
+fi
